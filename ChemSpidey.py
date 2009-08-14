846d8d969ba9cd837717196dba6a191b29ada844
Cameron Neylon <cameron.neylon@stfc.ac.uk>
2009-08-14 17:12:58 +0100

    Modified main wave script to pull and insert link to Chemspider page

--- ChemSpidey.py
+++ ChemSpidey.py
@@ -2,41 +2,36 @@
 #
 #!/usr/bin/python2.4
 #
-# Copyright 2009 Google Inc. All Rights Reserved.
-"""grauniady - a Guardian robot for Wave
+# 
+"""ChemSpidey - a ChemSpider robot for Wave.
 
-gives you the ten most recent items from The Guardian for text in a blip
+Gives you the an image of a named chemical for text in a blip. Heavily dependant on
+'Grauniady' the Guardian API Robot written by Chris Thorpe
 """
 
-__author__ = 'chris.thorpe@guardian.co.uk (Chris Thorpe)'
+__author__ = 'cameron.neylon@stfc.ac.uk (Cameron Neylon)'
 
 from waveapi import events
 from waveapi import robot
 
-from guardianapi import Client
+import ChemSpiPy
 
 
 def OnBlipSubmitted(properties, context):
 	blip = context.GetBlipById(properties['blipId'])
 	contents = blip.GetDocument().GetText()
-	if '?guardian' in contents:
-		search = '"%s"' % contents.replace('?guardian', '').replace('"', ' ').replace('\n', '')
-		items = Client('API_KEY_GOES_IN_HERE').search(q = search, count = '10', order_by_date = 'desc')
-		content = "\n\nLatest items from The Guardian for " + search + "\n"
-		for item in items:
-			standfirst = ""
-			try:
-				standfirst = item['standfirst'] + "\n"
-			except:
-				standfirst = ""
-			content = content + item['headline'] + ", by "+ item['byline'] +"\n" + standfirst + item['webUrl'] + "\n\n"
+	if '?chem' in contents:
+		query = '"%s"' % contents.replace('?chem', '').replace('"', ' ').replace('\n', '')
+		compound = ChemSpiPy.simplesearch(query)
+		url = "http://www.chemspider.com/Chemical-Structure.%s.html" % compound
+		content = query + "(" + url +")"
 		blip.GetDocument().SetText(content)
 
 
 
 if __name__ == '__main__':
-  grauniady = robot.Robot('grauniady',
+  ChemSpidey = robot.Robot('grauniady',
                          image_url='http://grauniady.appspot.com/public/grauniady.jpg',
                          profile_url='http://www.google.com')
-  grauniady.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
-  grauniady.Run(debug=True)
+  ChemSpidey.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
+  ChemSpidey.Run(debug=True)
